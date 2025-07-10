aa35a2c19659502df6fd095a67ec7b48c7da4580
Caleb Blondell <caleb.blondell@gmail.com>
2025-07-10 16:23:33 -0700

    chore: remove old files

--- rome_scripts/cyclone_source.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file://$HOME/.cyclonedds.xml
-export ROS_DOMAIN_ID=42
-source /opt/ros/jazzy/setup.bash
--- rome_scripts/launch_batons.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ros2 launch rome_py test_baton_launch.py
