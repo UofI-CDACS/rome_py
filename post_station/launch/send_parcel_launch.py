76f8300be4a43894fcac047f3609e2d82af949b1
MJHabermann <mbhabes11@gmail.com>
2025-07-18 15:45:24 -0700

    trying to merge

--- post_station/launch/send_parcel_launch.py
+++ post_station/launch/send_parcel_launch.py
@@ -1,31 +1,53 @@
 import json
 from launch import LaunchDescription
-from launch_ros.actions import Node
-import uuid
+from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-parcel_count = LaunchConfiguration('parcel_count', default=1)
-owner = LaunchConfiguration('owner', default='master')
-instruction_set = LaunchConfiguration('instruction_set', default='loop')
-data = LaunchConfiguration('data', default='')
+import uuid
+
 def generate_launch_description():
     #hostnames = ['rospi-1-desktop', 'rospi-2-desktop', 'rospi-3-desktop', 'rospi-4-desktop']  # simulated hostnames
     #stations = [hostname.replace('-', '_') + '/station_default' for hostname in hostnames]
     parcels = []
-    for i in range(parcel_count):
+    num_parcels = 4  # Adjust this number as needed for your stress test
+    """parcels = [
+        {
+            'parcel_id': str(uuid.uuid4()),
+            'owner_id': 'master',
+            'prev_location': 'master',
+            'next_location': 'rospi_1',
+            'instruction_set': 'loop',
+            'data': [
+                {'key': 'ttl', 'value': '10000'},
+                {'key': 'loop', 'value': 'true'}
+            ]
+        },
+    ]"""
+    for i in range(num_parcels):
         parcels.append({
             'parcel_id': str(uuid.uuid4()),
-            'owner_id': owner,
-            'prev_location': owner,
+            'owner_id': 'master',
+            'prev_location': 'master',
             'next_location': f'rospi_{(i % 4) + 1}',
-            'instruction_set': instruction_set,
+            'instruction_set': 'loop',
             'data': [
-            {'key': 'ttl', 'value': '100'}
+            {'key': 'ttl', 'value': '8'},
+            {'key': 'loop', 'value': 'true'}
             ]
         })
 
     parcels_json = json.dumps(parcels)
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Namespace to push'
+        ),
+
+        # Apply namespace to all nodes below
+        PushRosNamespace(LaunchConfiguration('namespace')),
+
         Node(
             package='post_station',
             executable='parcel',
