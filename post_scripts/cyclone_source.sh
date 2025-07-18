92c4200f1aa23cde509b610fc54bb85e617c705b
MJHabermann <mbhabes11@gmail.com>
2025-07-18 14:57:03 -0700

    Maybe fixed sourcing fast/cylcone dds

--- post_scripts/cyclone_source.sh
+++ post_scripts/cyclone_source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file://$(realpath "$1")/cyclonedds.xml
+export CYCLONEDDS_URI=file://$1/cyclonedds.xml
 export ROS_DOMAIN_ID=42
 source /opt/ros/jazzy/setup.bash
