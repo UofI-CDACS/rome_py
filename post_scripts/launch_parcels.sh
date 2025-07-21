91e7dd1ddf0cefb99f6ea145a523c083c82d3442
Caleb Blondell <caleb.blondell@gmail.com>
2025-07-21 16:06:28 -0700

    remove deprecated files

--- post_scripts/launch_parcels.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ros2 launch post_station send_parcel_launch.py
--- post_station/post_station/instruction_sets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import InstructionSet
-from .registry import register_instruction_set, INSTRUCTION_SET_REGISTRY as INSTRUCTION_SETS
-
-from . import loop
