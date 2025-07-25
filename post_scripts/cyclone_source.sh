459022beae2fe75af381d02810c6800d50bc381e
MJHabermann <mbhabes11@gmail.com>
2025-07-25 15:07:53 -0700

    Fixed DDS Sourcing for fast

--- post_scripts/cyclone_source.sh
+++ post_scripts/cyclone_source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI="$(pwd)/src/post/post_scripts/cyclonedds_unicast.xml"
+export CYCLONEDDS_URI="$(pwd)/src/post/post_scripts/cyclonedds.xml"
 export ROS_DOMAIN_ID=42
 source /opt/ros/jazzy/setup.bash
--- post_scripts/fast_source.sh
+++ post_scripts/fast_source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export RMW_IMPLEMENTATION=rmw_fastdds_cpp
-export CYCLONEDDS_URI=file://$1/fastdds.xml
+export CYCLONEDDS_URI="$(pwd)/src/post/post_scripts/fastdds.xml"
 export ROS_DOMAIN_ID=42
 source /opt/ros/jazzy/setup.bash
