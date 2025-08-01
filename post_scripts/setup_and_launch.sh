a5c45ff91ade3872511333d61feb13c439ede25f
MJHabermann <mbhabes11@gmail.com>
2025-08-01 10:15:51 -0700

    Fixed duplicate nodes

--- post_scripts/setup_and_launch.sh
+++ post_scripts/setup_and_launch.sh
@@ -63,6 +63,7 @@ launch_station_tmux_local() {
   remote_cmd+=" && set +u"
   remote_cmd+=" && source \"${ws_path}/install/setup.bash\""
   remote_cmd+=" && set -u"
+  remote_cmd+=" && pkill -f 'ros2 run post_core' || true"
   remote_cmd+=" && ros2 run post_core station"
   remote_cmd+=" --name ${node_name}"
   remote_cmd+=" --type ${node_type}"
@@ -144,6 +145,7 @@ if [[ "${SSH_PIS}" == "TRUE" ]]; then
     tmux kill-session -t post_launch
   fi
   for ip in "${!PI_USERS[@]}"; do
+
     launch_station_tmux_local "$ip" "${PI_USERS[$ip]}" "${PI_NAMES[$ip]}" "${PI_TYPES[$ip]}" "${WORKSPACE_FOLDER}" "${DDS_CONFIG_FILE}" "${QOS_PROFILE}" "${QOS_DEPTH}"
   done
   echo "Local tmux session 'post_launch' created with windows for each Pi."
