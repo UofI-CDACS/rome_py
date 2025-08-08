97616a179fe1b9c02478d788cf081fdf4078dc33
Caleb Blondell <caleb.blondell@gmail.com>
2025-08-08 15:19:05 -0700

    Add send logging

--- post_core/post_core/post_stations/base.py
+++ post_core/post_core/post_stations/base.py
@@ -1,4 +1,5 @@
 import asyncio
+from ..post_actions.registry import get_action
 import rclpy
 from rclpy.node import Node
 from post_interfaces.msg import Parcel, StationKill
@@ -64,10 +65,12 @@ class Station(Node):
             self._pub_cache[qos_key] = self.create_publisher(Parcel, topic_name, qos_profile)
         return self._pub_cache[qos_key]
 
-    def send_parcel(self, parcel, next_location: str):
+    async def send_parcel(self, parcel, next_location: str):
         topic = f'{next_location}/parcels'
         publisher = self.get_publisher(topic, self.qos_profile)
         publisher.publish(parcel)
+        log_parcel = get_action('file_log_parcel')(self, parcel)
+        await log_parcel(log_path=f"~/Desktop/test_ws/loop/{self.this_station}", is_sender_log=False)
 
     def _on_parcel_received(self, parcel):
         self.get_logger().info(f"Parcel received callback triggered for parcel {parcel.parcel_id}")
